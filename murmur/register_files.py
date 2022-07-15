1fb340e4d8e82fbd8da7c12cfad1ee9b6eacf3a3
paramir <prmr.paramir@gmail.com>
2022-07-15 13:34:25 +0900

    add: get_syncsafeメソッドを追加

--- murmur/register_files.py
+++ murmur/register_files.py
@@ -1,5 +1,4 @@
 import glob
-import re
 
 
 def enumerate_files(path: str) -> list[str]:
@@ -20,6 +19,11 @@ class ID3:
         self.disc_number = None
         self.year = None
 
+    def get_syncsafe(self, data: bytes) -> int:
+        data = [x & 0b01111111 for x in data]
+        ans = (data[0] << 21) | (data[1] << 14) | (data[2] << 7) | data[3]
+        return ans
+
     def load(self):
         with open(self.file_address, 'rb') as f:
             header = f.read(10)
@@ -28,4 +32,6 @@ class ID3:
             self.is_ID3v2 = True
             major_version = str(header[3])
             revision_number = str(header[4])
-            self.version = "ID3v2." + major_version + "." + revision_number
+            self.tag_version = "ID3v2." + major_version + "." + revision_number
+            self.tag_flags = header[5]
+            self.tag_size = self.get_syncsafe(header[6: 10])
--- tests/test_register_files.py
+++ tests/test_register_files.py
@@ -31,11 +31,22 @@ class TestEnumerateFiles(TestCase):
 class TestID3(TestCase):
     path = os.path.join(DATA_DIR, "id3v2.mp3")
 
+    def test_get_syncsafe(self):
+        id3v2 = ID3(self.path)
+        patterns = [
+            ([0b11111111, 0b11111111, 0b11111111, 0b11111111], 268435455),
+            ([0b00000000, 0b00000000, 0b00000000, 0b00000000], 0),
+            ([0b00111010, 0b01101111, 0b00011010, 0b00010101], 123456789),
+        ]
+        for in_data, out_data in patterns:
+            with self.subTest():
+                self.assertEqual(id3v2.get_syncsafe(in_data), out_data)
+
     def test_header(self):
         id3v2 = ID3(self.path)
         self.assertEqual(id3v2.is_ID3v2, True)
-        self.assertEqual(id3v2.version, "ID3v2.3.0")
-
+        self.assertEqual(id3v2.tag_version, "ID3v2.3.0")
+        self.assertEqual(id3v2.tag_size, 10426)
 
 if __name__ == '__main__':
     unittest.main()

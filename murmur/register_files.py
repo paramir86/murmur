889a98d2944e4d2c4c4f85c4f4dc8a752e6c7adf
paramir <prmr.paramir@gmail.com>
2022-07-15 12:15:11 +0900

    add: ID3ヘッダ読み込み処理追加(WIP)

--- murmur/register_files.py
+++ murmur/register_files.py
@@ -1,6 +1,31 @@
 import glob
+import re
 
 
 def enumerate_files(path: str) -> list[str]:
     safix = r"/**/*.mp3"
     return glob.glob(path + safix, recursive=True)
+
+
+class ID3:
+    def __init__(self, file_address: str):
+        self.file_address = file_address
+        self.load()
+
+        self.title = None
+        self.artist = None
+        self.album = None
+        self.album_artist = None
+        self.track = None
+        self.disc_number = None
+        self.year = None
+
+    def load(self):
+        with open(self.file_address, 'rb') as f:
+            header = f.read(10)
+
+        if header[0: 3] == b'ID3':
+            self.is_ID3v2 = True
+            major_version = str(header[3])
+            revision_number = str(header[4])
+            self.version = "ID3v2." + major_version + "." + revision_number

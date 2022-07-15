13791858ed423c284012294f60799c0cdfd1a025
paramir <prmr.paramir@gmail.com>
2022-07-15 15:48:21 +0900

    wip: タグ解釈機能作成中

--- murmur/register_files.py
+++ murmur/register_files.py
@@ -1,3 +1,4 @@
+from ast import parse
 import glob
 
 
@@ -26,8 +27,11 @@ class ID3:
 
     def load(self):
         with open(self.file_address, 'rb') as f:
-            header = f.read(10)
+            self.parse_header(f.read(10))
+            self.frames = f.read(self.tag_size)
+            self.parse_frames(self.frames)
 
+    def parse_header(self, header: bytes):
         if header[0: 3] == b'ID3':
             self.is_ID3v2 = True
             major_version = str(header[3])
@@ -35,3 +39,12 @@ class ID3:
             self.tag_version = "ID3v2." + major_version + "." + revision_number
             self.tag_flags = header[5]
             self.tag_size = self.get_syncsafe(header[6: 10])
+
+    def parse_frames(self, frames: bytes):
+        idx = 0
+        for i in range(7):
+            frame_id = frames[idx+0: idx+4]
+            frame_size = self.get_syncsafe(frames[idx+4: idx+8])
+            frame_flags = frames[idx+8: idx+10]
+            print(frame_id, frame_size, frame_flags, frames[idx+10: idx+10+frame_size])
+            idx += 10+frame_size

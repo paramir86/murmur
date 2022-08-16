a0ae4b19b5575366c5c13dd35db8135e2f6303d5
paramir86 <prmr.paramir@gmail.com>
2022-08-17 00:41:19 +0900

    add: save

--- backend/sql_app/models.py
+++ backend/sql_app/models.py
@@ -1,27 +1,41 @@
-from cgi import print_arguments
+from cgi import parse_multipart, print_arguments
+from code import interact
 from os import cpu_count
+from time import CLOCK_REALTIME
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
 from .database import Base
 
 
-# 全部適当。ちゃんと見直す。
-class Track(Base):
-    __tablename__ = "tracks"
+class Song(Base):
+    __tablename__ = "songs"
 
     id = Column(Integer, primary_key=True, index=True)
-    artist = relationship("Artist", back_populates="name")
-    album = relationship("Album", back_poplulates="name")
+    artist_id = Column(Integer, ForeignKey("artsits.id"))
+    album_id = Column(Integer, ForeignKey("artists.id"))
+    track_numer = Column(Integer)
+    track_denom = Column(Integer)
+    disc_numer = Column(Integer)
+    disc_denom = Column(Integer)
+    year = Column(Integer)
 
 
 class Artist(Base):
     __tablename__ = "artists"
 
     id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+
+    songs = relationship("Song", backref="artist")
+    albums = relationship("Album", backref="album_artist")
 
 
 class Album(Base):
     __tablename__ = "albums"
 
     id = Column(Integer, primary_key=True, index=True)
+    album_artist_id = Column(Integer, ForeignKey("artists.id"))
+    name = Column(String)
+
+    songs = relationship("Song", backref="album")
---
+++ backend/sql_app/schemas.py
@@ -0,0 +1,6 @@
+from typing import List, Union
+
+from pydantic import BaseModel
+
+class SongBase(BaseModel):
+    
